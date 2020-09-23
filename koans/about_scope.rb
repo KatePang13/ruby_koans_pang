0e191eb4d7f5b8bcadbbb891eec45a93ddd4ceda
katepang13 <liuyhpangpang@gmail.com>
2020-09-23 11:44:57 +0800

    Update about_scope.rb

--- koans/about_scope.rb
+++ koans/about_scope.rb
@@ -18,7 +18,7 @@ class AboutScope < Neo::Koan
   end
 
   def test_dog_is_not_available_in_the_current_scope
-    assert_raise(___) do
+    assert_raise(NameError) do
       Dog.new
     end
   end
@@ -26,11 +26,11 @@ class AboutScope < Neo::Koan
   def test_you_can_reference_nested_classes_using_the_scope_operator
     fido = Jims::Dog.new
     rover = Joes::Dog.new
-    assert_equal __, fido.identify
-    assert_equal __, rover.identify
+    assert_equal :jims_dog, fido.identify
+    assert_equal :joes_dog, rover.identify
 
-    assert_equal __, fido.class != rover.class
-    assert_equal __, Jims::Dog != Joes::Dog
+    assert_equal true, fido.class != rover.class
+    assert_equal true, Jims::Dog != Joes::Dog
   end
 
   # ------------------------------------------------------------------
@@ -38,42 +38,50 @@ class AboutScope < Neo::Koan
   class String
   end
 
+  # ClassName, 默认为当前 scope 的定义
   def test_bare_bones_class_names_assume_the_current_scope
-    assert_equal __, AboutScope::String == String
+    assert_equal true, AboutScope::String == String
   end
 
+  # ClassName, 默认为当前 scope 的定义
+  # "HI", 是一个 系统自建的 String 实例
   def test_nested_string_is_not_the_same_as_the_system_string
-    assert_equal __, String == "HI".class
+    assert_equal false, String == "HI".class
   end
 
+  # ::ClassName,指定为 全局 scope
   def test_use_the_prefix_scope_operator_to_force_the_global_scope
-    assert_equal __, ::String == "HI".class
+    assert_equal true, ::String == "HI".class
   end
 
   # ------------------------------------------------------------------
 
   PI = 3.1416
 
+  # 常量 定义为 首字母大写的名字
   def test_constants_are_defined_with_an_initial_uppercase_letter
-    assert_equal __, PI
+    assert_equal 3.1416, PI
   end
 
   # ------------------------------------------------------------------
 
   MyString = ::String
 
+  # ClassName 就是 常量
   def test_class_names_are_just_constants
-    assert_equal __, MyString == ::String
-    assert_equal __, MyString == "HI".class
+    assert_equal true, MyString == ::String
+    assert_equal true, MyString == "HI".class
   end
 
+  # 常量可以显式地查找  scope.const_get(strName)
   def test_constants_can_be_looked_up_explicitly
-    assert_equal __, PI == AboutScope.const_get("PI")
-    assert_equal __, MyString == AboutScope.const_get("MyString")
+    assert_equal true, PI == AboutScope.const_get("PI")
+    assert_equal true, MyString == AboutScope.const_get("MyString")
   end
 
+  # 可以获取 指定类或模块的 常量列表
   def test_you_can_get_a_list_of_constants_for_any_class_or_module
-    assert_equal __, Jims.constants
-    assert Object.constants.size > _n_
+    assert_equal [:Dog], Jims.constants
+    assert Object.constants.size > 1
   end
 end
