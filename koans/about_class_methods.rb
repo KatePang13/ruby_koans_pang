59833bdcf38f293f33ed5ba5725d98b05e55e037
katepang13 <liuyhpangpang@gmail.com>
2020-09-23 15:58:04 +0800

    Update about_class_methods.rb

--- koans/about_class_methods.rb
+++ koans/about_class_methods.rb
@@ -6,34 +6,37 @@ class AboutClassMethods < Neo::Koan
 
   def test_objects_are_objects
     fido = Dog.new
-    assert_equal __, fido.is_a?(Object)
+    assert_equal true, fido.is_a?(Object)
   end
 
   def test_classes_are_classes
-    assert_equal __, Dog.is_a?(Class)
+    assert_equal true, Dog.is_a?(Class)
   end
 
+  # class 也是 object
   def test_classes_are_objects_too
-    assert_equal __, Dog.is_a?(Object)
+    assert_equal true, Dog.is_a?(Object)
   end
 
   def test_objects_have_methods
     fido = Dog.new
-    assert fido.methods.size > _n_
+    assert fido.methods.size > 1
   end
 
   def test_classes_have_methods
-    assert Dog.methods.size > _n_
+    assert Dog.methods.size > 1
   end
 
+  #可以在已有的object上定义方法
   def test_you_can_define_methods_on_individual_objects
     fido = Dog.new
     def fido.wag
       :fidos_wag
     end
-    assert_equal __, fido.wag
+    assert_equal :fidos_wag, fido.wag
   end
 
+  # 在已有的object上定义方法，class的其他object不会受影响
   def test_other_objects_are_not_affected_by_these_singleton_methods
     fido = Dog.new
     rover = Dog.new
@@ -41,7 +44,7 @@ class AboutClassMethods < Neo::Koan
       :fidos_wag
     end
 
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       rover.wag
     end
   end
@@ -58,14 +61,16 @@ class AboutClassMethods < Neo::Koan
     :class_level_wag
   end
 
+  # class 也是 object, 所有也可以定义单例方法
   def test_since_classes_are_objects_you_can_define_singleton_methods_on_them_too
-    assert_equal __, Dog2.wag
+    assert_equal :class_level_wag, Dog2.wag
   end
 
+  # class方法 与 object方法 相互独立;
   def test_class_methods_are_independent_of_instance_methods
     fido = Dog2.new
-    assert_equal __, fido.wag
-    assert_equal __, Dog2.wag
+    assert_equal :instance_level_wag, fido.wag
+    assert_equal :class_level_wag, Dog2.wag
   end
 
   # ------------------------------------------------------------------
@@ -78,11 +83,12 @@ class AboutClassMethods < Neo::Koan
     @name
   end
 
+  # 类方法与实例不共享变量
   def test_classes_and_instances_do_not_share_instance_variables
     fido = Dog.new
     fido.name = "Fido"
-    assert_equal __, fido.name
-    assert_equal __, Dog.name
+    assert_equal "Fido", fido.name
+    assert_equal nil, Dog.name
   end
 
   # ------------------------------------------------------------------
@@ -93,8 +99,9 @@ class AboutClassMethods < Neo::Koan
     end
   end
 
+  # 类内部 可以定义 类方法
   def test_you_can_define_class_methods_inside_the_class
-    assert_equal __, Dog.a_class_method
+    assert_equal :dogs_class_method, Dog.a_class_method
   end
 
   # ------------------------------------------------------------------
@@ -102,9 +109,10 @@ class AboutClassMethods < Neo::Koan
   LastExpressionInClassStatement = class Dog
                                      21
                                    end
-
+  
+  # 类 statements 返回 最后一个 expression 的值
   def test_class_statements_return_the_value_of_their_last_expression
-    assert_equal __, LastExpressionInClassStatement
+    assert_equal 21, LastExpressionInClassStatement
   end
 
   # ------------------------------------------------------------------
@@ -113,8 +121,9 @@ class AboutClassMethods < Neo::Koan
                                  self
                                end
 
+  # 类内的 self 是 class object,而不是 实例                             
   def test_self_while_inside_class_is_class_object_not_instance
-    assert_equal __, Dog == SelfInsideOfClassStatement
+    assert_equal true, Dog == SelfInsideOfClassStatement
   end
 
   # ------------------------------------------------------------------
@@ -125,8 +134,9 @@ class AboutClassMethods < Neo::Koan
     end
   end
 
+  # 可以使用 self 代替 类名Dog 来定义类方法
   def test_you_can_use_self_instead_of_an_explicit_reference_to_dog
-    assert_equal __, Dog.class_method2
+    assert_equal :another_way_to_write_class_methods, Dog.class_method2
   end
 
   # ------------------------------------------------------------------
@@ -139,8 +149,9 @@ class AboutClassMethods < Neo::Koan
     end
   end
 
+  # 定义类方法的第三种方法  class << self
   def test_heres_still_another_way_to_write_class_methods
-    assert_equal __, Dog.another_class_method
+    assert_equal :still_another_way, Dog.another_class_method
   end
 
   # THINK ABOUT IT:
@@ -160,10 +171,10 @@ class AboutClassMethods < Neo::Koan
   # Are there times you might prefer one over the other?
 
   # ------------------------------------------------------------------
-
+  # 有一个从 实例方法中调用类方法的简单方式 instance.class.class_method
   def test_heres_an_easy_way_to_call_class_methods_from_instance_methods
     fido = Dog.new
-    assert_equal __, fido.class.another_class_method
+    assert_equal :still_another_way, fido.class.another_class_method
   end
 
 end
